326983f1bc2777d27f41fe3edf93e3e617131d51
Aaron Edaku <aaron.adroit@outlook.com>
2024-04-03 10:03:57 +0300

    Scaffolded Movie Model

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,7 +7,7 @@ namespace RazorPagesMovie.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         [DataType(DataType.Date)]
-        public DateTime Date { get; set; }
+        public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
         public decimal Price { get; set; }
     }
